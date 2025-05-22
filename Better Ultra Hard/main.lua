1d81200bd6232641234318378c4cbdafd2d66366
Lita Lavender <baron11704@yahoo.com>
2025-05-22 09:46:06 -0400

    Simplified Main Function

--- Better Ultra Hard/main.lua
+++ Better Ultra Hard/main.lua
@@ -4,10 +4,7 @@ local game = Game()
 local level = game:GetLevel()
 
 mod:AddCallback(ModCallbacks.MC_POST_UPDATE, function()
-  if game.Challenge ~= Challenge.CHALLENGE_ULTRA_HARD then return end
-  
-  local levelCurses = level:GetCurses()
-  if levelCurses & LevelCurse.CURSE_OF_THE_LOST == LevelCurse.CURSE_OF_THE_LOST then
+  if game.Challenge == Challenge.CHALLENGE_ULTRA_HARD then
     game.Challenge = Challenge.CHALLENGE_NULL
     level:RemoveCurses(LevelCurse.CURSE_OF_THE_LOST)
     game.Challenge = Challenge.CHALLENGE_ULTRA_HARD
